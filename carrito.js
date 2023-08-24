0c1cc51d91b97121b14b7df890ec9b88747fbd14
Lupe Melinger <melingerlupe@gmail.com>
2023-08-24 19:09:01 -0300

    Agrega funciones primera preentrega javascript

---
+++ carrito.js
@@ -0,0 +1,245 @@
+function displayStock(num){
+    if(num == 0) {
+        return 'Sin stock'
+    } 
+    else {
+        return 'Disponibles: ' + num
+    }
+}
+
+// Buzo anorak
+
+var cantidadBuzoAnorak = 0;
+
+function removeProduct(){
+    if(this.cantidadBuzoAnorak == 0){
+        console.log('button de borrar se deshabilita')
+    } 
+    else {
+        this. cantidadBuzoAnorak = this. cantidadBuzoAnorak-1
+        console.log('cantidad de productos actual',this.cantidad)
+    }
+}
+
+function addProduct(){
+    this.cantidadBuzoAnorak = this.cantidadBuzoAnorak+1
+    console.log("cantidad de productos actual", this.cantidadBuzoAnorak) 
+}
+
+
+// Blazer rework
+var cantidadBlazer = 0;
+
+function removeProduct(){
+    if(this.cantidadBlazer == 0){
+        console.log('button de borrar se deshabilita')
+    } 
+    else {
+        this. cantidadBlazer = this. cantidadBlazer-1
+        console.log('cantidad de productos actual',this.cantidadBlazer)
+    }
+}
+
+function addProduct(){
+    this.cantidadBlazer = this.cantidadBlazer+1
+    console.log("cantidad de productos actual", this.cantidadBlazer) 
+}
+
+// Campera flowers
+var cantidadCampera = 0;
+
+function removeProduct(){
+    if(this.cantidadCampera == 0){
+        console.log('button de borrar se deshabilita')
+    } 
+    else {
+        this. cantidadCampera = this. cantidadCampera-1
+        console.log('cantidad de productos actual',this.cantidadCampera)
+    }
+}
+
+function addProduct(){
+    this.cantidadCampera = this.cantidadCampera+1
+    console.log("cantidad de productos actual", this.cantidadCampera) 
+}
+
+// Mangas black
+var cantidadMangasB = 0;
+
+function removeProduct(){
+    if(this.cantidadMangasB == 0){
+        console.log('button de borrar se deshabilita')
+    } 
+    else {
+        this. cantidadMangasB = this. cantidadMangasB-1
+        console.log('cantidad de productos actual',this.cantidadMangasB)
+    }
+}
+
+function addProduct(){
+    this.cantidadMangasB = this.cantidadMangasB+1
+    console.log("cantidad de productos actual", this.cantidadMangasB) 
+}
+
+
+// Mangas candy
+var cantidadMangasC = 0;
+
+function removeProduct(){
+    if(this.cantidadMangasC == 0){
+        console.log('button de borrar se deshabilita')
+    } 
+    else {
+        this. cantidadMangasC = this. cantidadMangasC-1
+        console.log('cantidad de productos actual',this.cantidadMangasB)
+    }
+}
+
+function addProduct(){
+    this.cantidadMangasC = this.cantidadMangasC+1
+    console.log("cantidad de productos actual", this.cantidadMangasC) 
+}
+
+// Polera Crema
+var cantidadPoleraCrema = 0;
+
+function removeProduct(){
+    if(this.cantidadPoleraCrema == 0){
+        console.log('button de borrar se deshabilita')
+    } 
+    else {
+        this. cantidadPoleraCrema = this. cantidadPoleraCrema-1
+        console.log('cantidad de productos actual',this.cantidadMangasB)
+    }
+}
+
+function addProduct(){
+    this.cantidadPoleraCrema = this.cantidadPoleraCrema+1
+    console.log("cantidad de productos actual", this.cantidadPoleraCrema) 
+}
+
+// Polera Crema
+var cantidadPoleraCrema = 0;
+
+function removeProduct(){
+    if(this.cantidadPoleraCrema == 0){
+        console.log('button de borrar se deshabilita')
+    } 
+    else {
+        this. cantidadPoleraCrema = this. cantidadPoleraCrema-1
+        console.log('cantidad de productos actual',this.cantidadMangasB)
+    }
+}
+
+function addProduct(){
+    this.cantidadPoleraCrema = this.cantidadPoleraCrema+1
+    console.log("cantidad de productos actual", this.cantidadPoleraCrema) 
+}
+
+// Buzo floww
+var cantidadBuzoF = 0;
+
+function removeProduct(){
+    if(this.cantidadBuzoF == 0){
+        console.log('button de borrar se deshabilita')
+    } 
+    else {
+        this. cantidadBuzoF = this. cantidadBuzoF-1
+        console.log('cantidad de productos actual',this.cantidadMangasB)
+    }
+}
+
+function addProduct(){
+    this.cantidadBuzoF = this.cantidadBuzoF+1
+    console.log("cantidad de productos actual", this.cantidadBuzoF) 
+}
+
+// Polera piel
+var cantidadPoleraPiel = 0;
+
+function removeProduct(){
+    if(this.cantidadPoleraPiel == 0){
+        console.log('button de borrar se deshabilita')
+    } 
+    else {
+        this. cantidadPoleraPiel = this. cantidadPoleraPiel-1
+        console.log('cantidad de productos actual',this.cantidadMangasB)
+    }
+}
+
+function addProduct(){
+    this.cantidadPoleraPiel = this.cantidadPoleraPiel+1
+    console.log("cantidad de productos actual", this.cantidadPoleraPiel) 
+}
+
+// Reme Manazana
+var cantidadRemeM = 0;
+
+function removeProduct(){
+    if(this.cantidadRemeM == 0){
+        console.log('button de borrar se deshabilita')
+    } 
+    else {
+        this. cantidadRemeM = this. cantidadRemeM-1
+        console.log('cantidad de productos actual',this.cantidadMangasB)
+    }
+}
+
+function addProduct(){
+    this.cantidadRemeM = this.cantidadRemeM+1
+    console.log("cantidad de productos actual", this.cantidadRemeM) 
+}
+
+// Reme Work Patch
+var cantidadRemeWP = 0;
+
+function removeProduct(){
+    if(this.cantidadRemeWP == 0){
+        console.log('button de borrar se deshabilita')
+    } 
+    else {
+        this. cantidadRemeWP = this. cantidadRemeWP-1
+        console.log('cantidad de productos actual',this.cantidadMangasB)
+    }
+}
+
+function addProduct(){
+    this.cantidadRemeWP = this.cantidadRemeWP+1
+    console.log("cantidad de productos actual", this.cantidadRemeWP) 
+}
+
+// Top Patch
+var cantidadTopP = 0;
+
+function removeProduct(){
+    if(this.cantidadTopP == 0){
+        console.log('button de borrar se deshabilita')
+    } 
+    else {
+        this. cantidadTopP = this. cantidadTopP-1
+        console.log('cantidad de productos actual',this.cantidadMangasB)
+    }
+}
+
+function addProduct(){
+    this.cantidadTopP = this.cantidadTopP+1
+    console.log("cantidad de productos actual", this.cantidadTopP) 
+}
+
+// Buzo Batik
+var cantidadBuzoB = 0;
+
+function removeProduct(){
+    if(this.cantidadBuzoB == 0){
+        console.log('button de borrar se deshabilita')
+    } 
+    else {
+        this. cantidadBuzoB = this. cantidadBuzoB-1
+        console.log('cantidad de productos actual',this.cantidadMangasB)
+    }
+}
+
+function addProduct(){
+    this.cantidadBuzoB = this.cantidadBuzoB+1
+    console.log("cantidad de productos actual", this.cantidadBuzoB) 
+}
